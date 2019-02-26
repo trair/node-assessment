3b267e9555ab7e8265aea6b70dfac601e9d50559
Todd Rasband <todd.rasband@gmail.com>
2019-02-25 22:21:22 -0700

    notes

--- usersCtrl.js
+++ usersCtrl.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   getUserByID(req, res) {
     const {userID} = req.params
+    //try using a find here instead of a filter. saves you a step and gives of clearer intentions
     let user = userData.filter(person => person.id === Number(userID))
     if (user.length > 0) return res.status(200).send(user[0])
     return res.sendStatus(404)
@@ -51,10 +52,15 @@ module.exports = {
     const index = userData.findIndex(person => {
       return person.id === userID
     })
+    // not sure why this passed, but you only want a single =, and this would delete the id on the esisting user
     userData[index] === req.body
     res.status(200).send(userData)
   },
   newUser(req, res) {
+    // it would be a better idea to keep a running tracker of id's instead of going 1 higher than the last for a few reasons
+      //what if they are the first user, there would be no id to increment it from
+      //What if the array was not sorted, its possible you could create a duplicate id
+      //What if the last user was deleted, then you add a new one. They would technically have repeat id's even though the old one was deleted.
     const lastID = userData[userData.length - 1].id
     const userObj = req.body
     userObj.id = lastID + 1
@@ -63,6 +69,7 @@ module.exports = {
   },
   deleteUser(req, res) {
     const {userID} = req.params
+    // you could use a filter and overwrite the userData value, save you a few lines
     const index = userData.findIndex(person => {
       return person.id === Number(userID)
     })
